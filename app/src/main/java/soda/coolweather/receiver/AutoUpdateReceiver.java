dda379f1a5689a20585c037e3707078841f71cd9
hikobe8 <hikoeb8@163.com>
2016-11-29 23:38:04 +0800

    增加后台自动更新天气

---
+++ app/src/main/java/soda/coolweather/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,8 @@
+package soda.coolweather.receiver;
+
+/**
+ * Created by Ray on 2016/11/29.
+ */
+
+public class AutoUpdateReceiver {
+}
---
+++ app/src/main/java/soda/coolweather/service/AutoUpdateService.java
@@ -0,0 +1,8 @@
+package soda.coolweather.service;
+
+/**
+ * Created by Ray on 2016/11/29.
+ */
+
+public class AutoUpdateService {
+}
